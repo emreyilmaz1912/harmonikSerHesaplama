7cd7d83522414e02e794eb1a712c74a1ae0f793a
emreyilmaz1912 <emreyilmaz264@gmail.com>
2022-12-17 02:27:46 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+patika.dev | JavaPatikasi
+
+Java ile girilen sayının harmonik serisini bulan program yazacağız.
+
+Harmonik Seri Formülü :1+(1/2)+(1/3)+(1/n)
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Harmonik Sayı Hesaplama");
+        Scanner input= new Scanner(System.in);
+        System.out.print("N sayısını giriniz :");
+        int n = input.nextInt();
+        double result = 0.0;
+        for (double i = 1; i <= n; i++){
+            result += (1/i);
+
+        }
+        System.out.println(result);
+    }
+}
